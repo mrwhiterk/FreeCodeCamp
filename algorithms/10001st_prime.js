8ef4869c4e33ba71ff8b2d3fd6425c638ea45707
Ryan White <mrwhiterk@gmail.com>
2019-10-05 00:28:21 -0400

    finished truncate solution

---
+++ algorithms/10001st_prime.js
@@ -0,0 +1,22 @@
+function nthPrime(n) {
+  if (n === 1) {
+    return 2;
+  }
+
+  let prime = 3;
+
+  loop: while (n - 2) {
+    prime += 2;
+
+    for (let j = 3; j < prime; j += 2) {
+      if (prime % j === 0) {
+        continue loop;
+      }
+    }
+    n--;
+  }
+
+  return prime;
+}
+
+console.log(nthPrime(1000));
